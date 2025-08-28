239d2f6d2d8d25156cab161c809237c0d48e0004
muhammad chaerul hafiz <mchaerulhafiz@gmail.com>
2025-08-28 09:13:21 +0700

    update host running

--- backend/timesheet-backend/src/main.ts
+++ backend/timesheet-backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://10.8.60.35:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
